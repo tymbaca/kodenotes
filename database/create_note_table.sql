c3a1288165b0d90cc69111fd111e4afcf8ca8d25
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-08-16 20:38:25 +0400

    fix: now works fine

--- database/create_note_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS note (
-    id      INTEGER,
-    user_id INTEGER,
-    text    TEXT()
-)
--- database/postgres.go
+++ database/postgres.go
@@ -11,62 +11,102 @@ import (
 
 // PostgresDatabase implements Database interface. Connects to
 type PostgresDatabase struct {
-        *sql.DB
+	*sql.DB
 }
 
 // NewPostgresDatabase creates new PostgresDatabase object and connects to PostgreSQL server.
-// Please DON'T use whitespaces and backslashes in credentials (it is possible but unwanted).
-// NEVER user tailing backslashes.
-func NewPostgresDatabase(addr, user, password string) (*PostgresDatabase, error) {
-        // Details: 34.1.2 https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-        connStr := fmt.Sprintf("host='%s' port=5432 user='%s' password='%s' sslmode=disable",
-                addr, user, password)
-        db, err := sql.Open("postgres", connStr)
-        if err != nil {
-                return nil, err
-        }
+// DON'T use whitespaces and backslashes in credentials.
+func NewPostgresDatabase(host, password string) (*PostgresDatabase, error) {
+	// Details: 34.1.2 https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+	connStr := fmt.Sprintf("host=%s dbname=postgres user=postgres password=%s sslmode=disable", host, password)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
 
-        err = db.Ping()
-        if err != nil {
-                return nil, err
-        }
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
 
-        pg := &PostgresDatabase{db}
+	pg := &PostgresDatabase{db}
 
-        err = pg.init()
-        if err != nil {
-                return nil, err
-        }
+	err = pg.init()
+	if err != nil {
+		return nil, err
+	}
 
-        return pg, nil
+	return pg, nil
 }
 
 func (s *PostgresDatabase) init() error {
-        err := s.createNotesTable()
-        if err != nil {
-                return err
-        }
-        return nil
+        err := s.addUuidExtension()
+	if err != nil {
+		return err
+	}
+
+	err = s.createNotesTable()
+	if err != nil {
+		return err
+	}
+
+	err = s.createUsersTable()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *PostgresDatabase) addUuidExtension() error {
+	query := `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
+
+	_, err := s.Exec(query)
+	if err != nil {
+		return err
+	}
+	return nil
+        
+}
+
+func (s *PostgresDatabase) createUsersTable() error {
+	query := `
+        CREATE TABLE IF NOT EXISTS users (
+                id              UUID DEFAULT uuid_generate_v1(),
+                username        VARCHAR(250) NOT NULL,
+                password        VARCHAR(250) NOT NULL,
+
+                PRIMARY KEY (id)
+        );`
+
+	_, err := s.Exec(query)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *PostgresDatabase) createNotesTable() error {
-        query := `
-        CREATE TABLE IF NOT EXISTS note (
-                note_id UUID,
+	query := `
+        CREATE TABLE IF NOT EXISTS notes (
+                id      UUID DEFAULT uuid_generate_v1(),
                 user_id UUID,
-                text    TEXT
-        )`
+                text    TEXT,
 
-        _, err := s.Exec(query)
-        if err != nil {
-                return err
-        }
-        return nil
+                PRIMARY KEY (id),
+                CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES "users"(id)
+        );`
+
+	_, err := s.Exec(query)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *PostgresDatabase) GetNotes(userId int) NoteGetAll {
-        var notes NoteGetAll 
-        return notes
+	var notes NoteGetAll
+	return notes
 }
 
 func (s *PostgresDatabase) PostNote(userId int, note NoteCreate) {}
