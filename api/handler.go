8385888c5ac77ae549186a520442a66ba858c88a
tymbaca <tumb4ka228@gmail.com>
2023-08-16 14:04:23 +0200

    A little renaming

--- api/handler.go
+++ api/handler.go
@@ -16,21 +16,21 @@ func (s *Server) handleNotes(w http.ResponseWriter, r *http.Request) {
 
         switch r.Method {
         case http.MethodGet:
-                s.GetNotes(w, r, userId)
+                s.handleGetNotes(w, r, userId)
         case http.MethodPost:
-                s.PostNote(w, r, userId)
+                s.handlePostNote(w, r, userId)
         default:
                 http.Error(w, fmt.Sprintf("method %s not allowed. Use eather GET or POST", r.Method), http.StatusMethodNotAllowed)
         }
 }
 
 
-func (s *Server) GetNotes(w http.ResponseWriter, r *http.Request, userId int) {
-        notes := s.db.GetNotes(userId)
+func (s *Server) handleGetNotes(w http.ResponseWriter, r *http.Request, userId int) {
+        // notes := s.db.GetNotes(userId)
         json.NewEncoder(w)
 }
 
-func (s *Server) PostNote(w http.ResponseWriter, r *http.Request, userId int) {
+func (s *Server) handlePostNote(w http.ResponseWriter, r *http.Request, userId int) {
         textBytes, err := io.ReadAll(r.Body)
         text := string(textBytes)
         if err != nil {
