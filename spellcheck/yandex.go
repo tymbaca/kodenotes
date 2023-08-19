7288cfb1273848fde9e52e4589fede979b7a2456
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-08-19 13:04:19 +0400

    fix: yandex.speller url get env

--- spellcheck/yandex.go
+++ spellcheck/yandex.go
@@ -1,16 +1,15 @@
 package spellcheck
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
-	"io"
-	"net/http"
-	"net/url"
-	"os"
-	"time"
+        "context"
+        "encoding/json"
+        "errors"
+        "io"
+        "net/http"
+        "net/url"
+        "time"
 
-	"github.com/tymbaca/kodenotes/util"
+        "github.com/tymbaca/kodenotes/util"
 )
 
 const (
@@ -20,41 +19,40 @@ const (
 var (
         ErrYandexTooBigText = errors.New("text is too big: pass text with less than 10_000 characters")
 
-        yandexSpellerUrl = os.Getenv(yandexSpellerUrlEnvVar)
+        yandexSpellerUrl      = util.MustGetenv(yandexSpellerUrlEnvVar)
         yandexResponseTimeout = util.GetenvIntOrDefault("YANDEX_SPELLER_TIMEOUT", 10)
 )
 
 type ResponseWrapper struct {
         resp *http.Response
-        err   error
+        err  error
 }
 
-type YandexSpeller struct {}
+type YandexSpeller struct{}
 
 func NewYandexSpeller() *YandexSpeller {
         yandexSpeller := &YandexSpeller{}
         return yandexSpeller
 }
 
-// Check sends text to Yandex.Speller (https://yandex.ru/dev/speller) to check it for spelling mistakes. 
-// Returns: 
-//      - CheckResponse, nil - if spelling mistake found
-//      - nil, error - if unexcpected error accured during the request. Error can be ErrTooBigText or another.
-//
+// Check sends text to Yandex.Speller (https://yandex.ru/dev/speller) to check it for spelling mistakes.
+// Returns:
+//   - CheckResponse, nil - if spelling mistake found
+//   - nil, error - if unexcpected error accured during the request. Error can be ErrTooBigText or another.
 func (y *YandexSpeller) Check(text string) (CheckResponse, error) {
 
         if len(text) >= 10_000 {
                 return nil, ErrYandexTooBigText
         }
 
-        var formData url.Values
+        formData := url.Values{}
         formData.Add("text", text)
 
         resp, err := fetchCheckData(formData)
         if err != nil {
                 return nil, err
         }
-        
+
         data, err := io.ReadAll(resp.Body)
         if err != nil {
                 return nil, err
@@ -71,15 +69,15 @@ func (y *YandexSpeller) Check(text string) (CheckResponse, error) {
 }
 
 func fetchCheckData(formData url.Values) (*http.Response, error) {
-        ctx, cancel := context.WithTimeout(context.Background(), time.Duration(yandexResponseTimeout) * time.Second)
+        ctx, cancel := context.WithTimeout(context.Background(), time.Duration(yandexResponseTimeout)*time.Second)
         defer cancel()
-        respWrapCh      := make(chan ResponseWrapper)
+        respWrapCh := make(chan ResponseWrapper)
 
         go func() {
                 resp, err := http.PostForm(yandexSpellerUrl, formData)
                 respWrapCh <- ResponseWrapper{resp: resp, err: err}
         }()
-        
+
         for {
                 select {
                 case <-ctx.Done():
@@ -90,4 +88,3 @@ func fetchCheckData(formData url.Values) (*http.Response, error) {
 
         }
 }
-
