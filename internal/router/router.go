daefe305f020ad808e8df571e2a26e083c1b93c7
tymbaca <tumb4ka228@gmail.com>
2023-08-23 16:15:23 +0200

    feat: more work on router
    - Global and pattern-local Middleware support
    - Handle + Get, Post, etc shortcuts
    - something else

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,27 +1,89 @@
 package router
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
-type Middleware func(http.HandlerFunc) http.HandlerFunc
+type Middleware func(next http.HandlerFunc) http.HandlerFunc
 
 type Router struct {
 	routes map[string]Route
-	// Middlewares for all requests. Executes before Route middlewares
-	middlewares []Middleware
+
+	// Middlewares for all requests. Executes before routesMiddlewares
+	//
+	// Executes in order: req -> third(second(first(myhandler)))
+	globalMiddlewares []Middleware
+
+	// Map with middlewares for each pattern. Pattern is key, middleware is
+	// value. Executes before localMiddlewaresMap
+	//
+	// Executes in order: req -> third(second(first(myhandler)))
+	localMiddlewaresMap map[string][]Middleware
 }
 
 func Default() *Router {
 	return &Router{
-		routes:      nil,
-		middlewares: {Logger},
+		routes:            nil,
+		globalMiddlewares: []Middleware{Logger},
 	}
 }
 
 func New(middlewares ...Middleware) *Router {
 	return &Router{
-		routes:      nil,
-		middlewares: middlewares, // TODO: in which order thay will be used?
+		routes:            nil,
+		globalMiddlewares: middlewares, // TODO: in which order thay will be used?
 	}
+}
+
+func Run(addr string) error
+
+func (r *Router) Handle(method, pattern string, handler http.HandlerFunc) {
+	route, ok := r.routes[pattern]
+	if ok {
+		// pattern already registered
+		route.handlers[method] = handler
+	} else {
+		// new pattern
+		r.routes[pattern] = NewRoute()
+		route := r.routes[pattern]
+
+		route.handlers[method] = handler
+	}
+}
+
+// Get is a shortcut to r.Handle(http.MethodGet, ...)
+func (r *Router) Get(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodGet, pattern, handler)
+}
+
+// Post is a shortcut to r.Handle(http.MethodPost, ...)
+func (r *Router) Post(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodPost, pattern, handler)
+}
+
+// Post is a shortcut to r.Handle(http.MethodPut, ...)
+func (r *Router) Put(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodPut, pattern, handler)
+}
+
+// Post is a shortcut to r.Handle(http.MethodPatch, ...)
+func (r *Router) Patch(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodPatch, pattern, handler)
+}
+
+// Post is a shortcut to r.Handle(http.MethodDelete, ...)
+func (r *Router) Delete(pattern string, handler http.HandlerFunc) {
+	r.Handle(http.MethodDelete, pattern, handler)
+}
+
+func (r *Router) Use(middleware ...Middleware) {
+	r.globalMiddlewares = append(r.globalMiddlewares, middleware...)
+}
+
+func (r *Router) UseLocal(pattern string, middleware ...Middleware) {
+	r.localMiddlewaresMap[pattern] = append(r.localMiddlewaresMap[pattern], middleware...)
 }
 
 type Route struct {
@@ -31,9 +93,16 @@ type Route struct {
 	middlewares []Middleware
 }
 
-func (route *Route) getHandlerWithMethod() http.HandlerFunc {
+func NewRoute() Route {
+	return Route{
+		handlers:    map[string]http.HandlerFunc{},
+		middlewares: []Middleware{},
+	}
+}
+
+func (rt *Route) getHandlerWithMethod() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		handler, ok := route.handlers[r.Method]
+		handler, ok := rt.handlers[r.Method]
 		if !ok {
 			http.Error(w, `"detail": "method not allowed"`, http.StatusMethodNotAllowed)
 		}
@@ -49,44 +118,41 @@ func (rt *Route) GetHandlerFunc() http.HandlerFunc {
 	return handler
 }
 
-func (r *Router) Handle(method, pattern string, handler http.HandlerFunc, middlewares ...Middleware) {
-	route, ok := r.routes[pattern]
-	if ok {
-		// pattern already registered
-		route.handlers[method] = handler
-		routes
+func example() {
+	getHandler := func(w http.ResponseWriter, r *http.Request) {}
+	postHandler := func(w http.ResponseWriter, r *http.Request) {}
+
+	var timeRecorder Middleware = func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			startTime := time.Now()
+			next(w, r)
+			endTime := time.Now()
+			duration := startTime.Sub(endTime)
+			w.Header().Set("X-Duration", duration.String())
+		}
 	}
-}
 
-// Get is a shortcut to r.Handle(http.MethodGet, ...)
-func (r *Router) Get(pattern string, handler http.HandlerFunc, middlewares ...Middleware) {
-	r.Handle(http.MethodGet, pattern, handler, middlewares...)
-}
+	var authorize Middleware = func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			_, _, ok := r.BasicAuth()
+			if !ok {
+				http.Error(w, `"detail": "set basic auth"`, http.StatusBadRequest)
+			}
+			// check if credentials are correct...
+			next(w, r)
+		}
+	}
 
-// Post is a shortcut to r.Handle(http.MethodPost, ...)
-func (r *Router) Post(pattern string, handler http.HandlerFunc, middlewares ...Middleware) {
-	r.Handle(http.MethodPost, pattern, handler, middlewares...)
-}
-
-// Post is a shortcut to r.Handle(http.MethodPut, ...)
-func (r *Router) Put(pattern string, handler http.HandlerFunc, middlewares ...Middleware) {
-	r.Handle(http.MethodPut, pattern, handler, middlewares...)
-}
-
-// Post is a shortcut to r.Handle(http.MethodPatch, ...)
-func (r *Router) Patch(pattern string, handler http.HandlerFunc, middlewares ...Middleware) {
-	r.Handle(http.MethodPatch, pattern, handler, middlewares...)
-}
-
-// Post is a shortcut to r.Handle(http.MethodDelete, ...)
-func (r *Router) Delete(pattern string, handler http.HandlerFunc, middlewares ...Middleware) {
-	r.Handle(http.MethodDelete, pattern, handler, middlewares...)
-}
-
-func GetHandler(handler http.HandlerFunc) http.HandlerFunc {
-	return
-}
-
-func main() {
 	router := New(Logger)
+
+	// Set global middleware
+	router.Use(timeRecorder)
+
+	router.Get("/posts", getHandler)
+	router.Post("/posts", postHandler)
+
+	// Set middleware for "/posts" pattern
+	router.UseLocal("/posts", authorize) // WARN: what about '/posts/my' request? is will ignore it
+
+	log.Fatal(Run(":8080"))
 }
